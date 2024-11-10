6ccecf0f3ef7aa1160712b638f45efe9295177f3
Karthik P K <kpunichathayak@gmail.com>
2024-11-10 15:52:00 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,11 @@ import RootRouter from "./routes/index.js"
 const PORT = 3000;
 
 const app = express();
-app.use(json())
 app.use(cors({
     origin: ["https://mit-prod-fra3.vercel.app"],
     credentials: true
 }))
+app.use(json())
 app.use("/api/v1", RootRouter);
 
 
